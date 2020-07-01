e39ee199d7b7a9cb40821b8cb7f52082c2537ac6
HsQuQu <HQ4uqu@gmail.com>
2020-07-01 22:09:08 +0300

    updated

--- Library/ArtAttack.js
+++ Library/ArtAttack.js
@@ -1,53 +1,27 @@
 //******************************* Art Attack Style Sheet *******************************
 
 
-//******************************** Global ********************************
+//******************************* Global ********************************
 
-// Login
 function login() {
-    var body = document.getElementsByName("body");
-    var x = document.getElementById("loginHolder");
-    if (x.style.display === "none") {
-        x.style.display = "block";
-        body.style.overflow="hidden";
-    } else {
-        x.style.display = "none";
-    }
+    document.getElementById("loginHolder").style.display= "block";
 }
-
 function signup() {
-    var x = document.getElementById("signupHolder");
-    if (x.style.display === "none") {
-        x.style.display = "block";
-    } else {
-        x.style.display = "none";
-    }
+    document.getElementById("signupHolder").style.display = "block";
 }
-
 function RPass() {
-        document.getElementById("RPassHolder").style.display= "block";
-        document.getElementById("loginHolder").style.display="none";
+    document.getElementById("RPassHolder").style.display = "block";
+    document.getElementById("loginHolder").style.display = "none";
 }
 
 function closeIN() {
-    var x = document.getElementById("loginHolder");
-    if (x.style.display === "none") {
-        x.style.display = "block";
-    } else {
-        x.style.display = "none";
-    }
+    document.getElementById("loginHolder").style.display = "none";
 }
 function closeUp() {
-    var x = document.getElementById("signupHolder");
-    if (x.style.display === "none") {
-        x.style.display = "block";
-    } else {
-        x.style.display = "none";
-    }
+    document.getElementById("signupHolder").style.display = "none";
 }
-
 function closeRPass() {
-    document.getElementById("RPassHolder").style.display="none";
+    document.getElementById("RPassHolder").style.display = "none";
 }
 
 // Home Script Section
