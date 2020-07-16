b6988c07181aa5d1604f1547b95a0beae3b7b0df
doaa-ahmaad <d3d3Ahmaad@outlook.sa>
2020-07-16 12:52:09 +0300

    doaa page

--- Resource/Service2-1.js
+++ Resource/Service2-1.js
@@ -1,4 +1,9 @@
 // this js is for the service2-1 ONLY functions that work
 
+// this is for the help to the user
+$("#helpDesignBtn").one("click",function (){
+   alert("Hi , you can design anything you want in simplist way " +
+       "first Start with Template choose that apper in the bottom of the page or you can choose your custom wallpaeper" +
+       "then adding the shapes you want also the text that you want ")
+})
 
-$("#helpDesignBtn").onclick
