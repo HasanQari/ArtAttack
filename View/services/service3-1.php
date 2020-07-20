a282d64887977d1ab19c49532efc92fe3893b743
NoodM <s437001919@st.uqu.edu.sa>
2020-07-20 13:15:56 +0300

    Add Image PHP

--- View/services/service3-1.php
+++ View/services/service3-1.php
@@ -151,7 +151,7 @@ session_start();
                 }
                 ?>
                 <form action="upload.php" method="post" enctype="multipart/form-data">
-                    Select image to upload:
+
                     <input type="file" name="fileToUpload" id="fileToUpload" accept="image/png, image/jpeg , image/jpg" size="10">
                 </form>
             </div>
